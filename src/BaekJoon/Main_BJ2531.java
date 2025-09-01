f2673eb733650bf0613664f2a895acdc2a9482f9
etoile0626 <etoile0626@naver.com>
2025-09-02 04:25:00 +0900

    회전 초밥

---
+++ src/BaekJoon/Main_BJ2531.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    회전 초밥이 컨베이어 벨트 위에서 시계 방향으로 돌고 있음
+    벨트 위에는 같은 종류의 초밥이 2개 이상 존재할 수 없음
+    회전 초밥 집에서 2가지 행사를 진행
+    1. 벨트의 임의의 한 위치에서부터 k개의 접시를 연속해서 먹을 경우 할인된 정액 가격으로 제공
+    2. 각 고객에게 초밥의 종류가 쓰인 쿠폰을 발행, 1번 행사에 참여한 경우 이 쿠폰에 적혀진 초밥을 추가로 무료 제공. 벨트 위에 해당 초밥이 없으면 새로 만들어 제공.
+    위 행사에 참여하여 최대한 다양한 종류의 초밥을 먹으려고 함.
+    *회전 초밥 벨트에 놓인 접시의 수 n, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
+    *2 ≤ n ≤ 30,000, 2 ≤ d ≤ 3,000, 2 ≤ k ≤ 3,000 (k ≤ N), 1 ≤ c ≤ d
+ */
+
+public class Main_BJ2531 {                                          //회전 초밥
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
