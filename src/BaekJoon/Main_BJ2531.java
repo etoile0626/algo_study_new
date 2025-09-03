b94c299b8bc12fe7a95af34d19381df178620494
etoile0626 <etoile0626@naver.com>
2025-09-03 20:45:48 +0900

    회전 초밥

--- src/BaekJoon/Main_BJ2531.java
+++ src/BaekJoon/Main_BJ2531.java
@@ -21,5 +21,54 @@ public class Main_BJ2531 {                                          //회전 초
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken());                   //초밥의 총 갯수
+        int d = Integer.parseInt(st.nextToken());                   //초밥의 가짓수
+        int k = Integer.parseInt(st.nextToken());                   //연속으로 먹는 접시의 수
+        int c = Integer.parseInt(st.nextToken());                   //쿠폰 번호
+
+        //회전 초밥
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] sushi = new int[d + 1];
+        int cnt = 0;
+        //0번부터 k개 초밥 먹는 경우부터 산정
+        for(int i = 0; i < k; i++){
+            if(sushi[arr[i]] == 0){
+                cnt++;                      //처음 먹는 초밥이면 먹은 초밥 증가
+            }
+
+            sushi[arr[i]]++;                //해당 초밥 먹은거니까 수 증가
+        }
+
+        int max = cnt;                            //먹은 초밥 종류 최댓값 초기화
+
+        for(int i = 1; i < n; i++){
+            //이전 탐색의 쿠폰 사용 여부 검사
+            if(max <= cnt) {
+                if (sushi[c] == 0) {                         //쿠폰 사용 가능하면 사용하기
+                    max = cnt + 1;
+                } else{
+                    max = cnt;
+                }
+            }
+            
+            //다음 end 포인트로 이동
+            int end = (i + k - 1) % n;
+            if(sushi[arr[end]] == 0){
+                cnt++;
+            }
+            sushi[arr[end]]++;
+
+            //이전 start 포인트 값 제외
+            sushi[arr[i-1]]--;
+            if(sushi[arr[i-1]] == 0){
+                cnt--;
+            }
+        }
+
+        System.out.println(max);
     }
 }
