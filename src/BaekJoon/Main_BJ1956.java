7feec4f8ae057b6d66cf31ba465b9f29c230890e
etoile0626 <etoile0626@naver.com>
2025-09-07 21:50:12 +0900

    운동

---
+++ src/BaekJoon/Main_BJ1956.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    d
+ */
+
+public class Main_BJ1956 {                              //운동
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
