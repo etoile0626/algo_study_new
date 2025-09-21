999007c5680086c4d4762672d956748966fa9f44
etoile0626 <etoile0626@naver.com>
2025-09-22 00:12:12 +0900

    N과 M (10)

--- src/BaekJoon/Main_BJ15664.java
+++ src/BaekJoon/Main_BJ15664.java
@@ -3,6 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
@@ -12,12 +13,53 @@ import java.util.StringTokenizer;
 
 public class Main_BJ15664 {                         //N과 M (10)
 
-    static int n =
+    static int n, m;
+    static int[] arr, tmp;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    static void dfs(int cnt, int idx){
+        if(cnt == m){
+            for(int i = 0; i < m; i++){
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        int prev = -1;
+        for(int i = idx; i < n; i++){                       //내림차순 유지를 위해 idx 부터 시작
+            if(!visited[i] && arr[i] != prev) {
+                visited[i] = true;
+                tmp[cnt] = arr[i];
+                dfs(cnt + 1, i + 1);                //i번째를 채택했으므로 i + 1부터 탐색
+                visited[i] = false;
+                prev = arr[i];
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        n =
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        tmp = new int[m];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0, 0);
+
+        System.out.println(sb.toString());
     }
 }
