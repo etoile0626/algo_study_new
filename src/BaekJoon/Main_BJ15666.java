440a40f156b9bea2ef23f8c15d031b110b4adee1
etoile0626 <etoile0626@naver.com>
2025-09-23 00:07:53 +0900

    N과 M (12)

---
+++ src/BaekJoon/Main_BJ15666.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    n개 중 m개를 고른 수열 모두 출력
+    *같은 수를 여러번 골라도 됨
+    *오름차순으로 고르기
+    *중복 수열은 한 번만, 사전 순으로 증가하는 순서로 출력
+ */
+
+public class Main_BJ15666 {                     //N과 M (12)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
