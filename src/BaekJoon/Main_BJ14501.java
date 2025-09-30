8fe895d7b7132e89fdb548924ddb7d0e6d33bfe5
etoile0626 <etoile0626@naver.com>
2025-09-30 21:48:55 +0900

    퇴사

---
+++ src/BaekJoon/Main_BJ14501.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+   퇴사를 하려고 하는데 n + 1일째 되는 날 퇴사를 하기 위해서, 남은 n일 동안 최대한 많은 상담을 하려고 함
+
+ */
+
+public class Main_BJ14501 {                 //퇴사
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+    }
+}
