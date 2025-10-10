9842f6904385687b93bf7e12d14951811a979a4c
etoile0626 <etoile0626@naver.com>
2025-10-11 00:14:20 +0900

    .

---
+++ src/BaekJoon/Main_BJ2741.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Main_BJ2741 {                  // N 찍기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
