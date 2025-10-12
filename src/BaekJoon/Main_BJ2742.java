80815629d84575523db04830466b52e7a4969e39
etoile0626 <etoile0626@naver.com>
2025-10-12 22:42:22 +0900

    .

---
+++ src/BaekJoon/Main_BJ2742.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Main_BJ2742 {                  //기찍 N
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = n; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+}
