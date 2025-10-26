d94aa2003ab5a7d7c46039eecd716275a66474e9
etoile0626 <etoile0626@naver.com>
2025-10-26 15:38:18 +0900

    후보 추천하기

--- src/BaekJoon/Main_BJ1713.java
+++ src/BaekJoon/Main_BJ1713.java
@@ -1,7 +1,12 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main_BJ1713 {                      //후보 추천하기
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
     }
 }
