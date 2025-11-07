bc7536aef1742bf44401f3a1459c6eccbf2404e3
etoile0626 <etoile0626@naver.com>
2025-11-07 12:40:06 +0900

    2차원 배열의 합

---
+++ src/BaekJoon/Main_BJ2167.java
@@ -0,0 +1,48 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ2167 {                  //2차원 배열의 합
+    //다시 풀기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+
+        // 배열 초기화
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < k; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int sum = 0;
+
+            // (i,j)부터 (x,y)까지 더하기
+            for (int p = i; p <= x; p++) {
+                for (int q = j; q <= y; q++) {
+                    sum += arr[p][q];
+                }
+            }
+            sb.append(sum + "\n");
+        }
+
+        System.out.print(sb);
+    }
+}
