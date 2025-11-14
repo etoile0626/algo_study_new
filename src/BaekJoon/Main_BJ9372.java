567a4ea6c508e3150f2143a3c2c70c50bc44af9c
etoile0626 <etoile0626@naver.com>
2025-11-14 12:28:18 +0900

    상근이의 여행

---
+++ src/BaekJoon/Main_BJ9372.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ9372 {                          //상근이의 여행
+    //다시 풀기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int t = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < m; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+            }
+
+            System.out.println(n - 1);
+        }
+    }
+}
