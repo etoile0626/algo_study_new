c833420c03c31760484b998b4eea71a603d8f716
etoile0626 <etoile0626@naver.com>
2025-11-18 12:03:20 +0900

    소인수분해

---
+++ src/BaekJoon/Main_BJ11653.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Main_BJ11653 {                 //소인수분해
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {	// 또는 i * i <= N
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+        if (n != 1) {
+            System.out.println(n);
+        }
+    }
+}
