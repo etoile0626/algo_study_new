cad2c8890ff953752cc4d6d5672cfec520da489a
etoile0626 <etoile0626@naver.com>
2025-11-25 13:26:49 +0900

    LCM

---
+++ src/BaekJoon/Main_BJ5347.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ5347 {                      //LCM
+
+    public static long gcd(int x, int y) {
+        if (x % y == 0)
+            return y;
+        return gcd(y, x % y);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println((long) a * b / gcd(a, b));
+        }
+    }
+}
