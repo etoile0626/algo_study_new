6ca7a8c9ba8f6e546dbe92522ac893851d3cedc1
etoile0626 <etoile0626@naver.com>
2025-12-26 18:56:33 +0900

    숫자 카드 2

---
+++ src/BaekJoon/Main_BJ10816.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main_BJ10816 {                     //숫자 카드 2
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(st.nextToken());
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++){
+            int answer = Integer.parseInt(st.nextToken());
+
+            sb.append(map.getOrDefault(answer, 0)).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
