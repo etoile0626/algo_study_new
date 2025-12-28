231f14a7c4282664b4e309020b59ac9bfda32027
etoile0626 <etoile0626@naver.com>
2025-12-28 18:34:26 +0900

    서로다른 부분 문자열의 개수

---
+++ src/BaekJoon/Main_BJ11478.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main_BJ11478 {                     //서로 다른 부분 문자열의 개수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+}
