fbf3e4533bbe136ab89b3761ffe22da48d1a2bd2
etoile0626 <etoile0626@naver.com>
2026-01-05 12:57:02 +0900

    123

---
+++ src/BaekJoon/Main_BJ2740.java
@@ -0,0 +1,60 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ2740 {              //행렬 곱셈
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        // A행렬 입력
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        st = new StringTokenizer(br.readLine());
+
+        // B행렬 입력
+        st.nextToken();		// 어차피 M값으로 같은 수이기 때문에 버려도 상관 없다.
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] B = new int[m][K];
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        // 행렬 계산 및 출력
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < K; j++) {
+                int sum = 0;
+                for(int k = 0; k < m; k++) {
+                    sum += A[i][k] * B[k][j];
+                }
+                // A의 i행의 j열 연산이 끝나면 바로 출력문으로 보내준다.
+                sb.append(sum).append(' ');
+            }
+
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
