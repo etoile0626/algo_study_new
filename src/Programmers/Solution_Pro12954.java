4fd0f77ade08c3f8ef3e86576ffc7e4375722000
etoile0626 <etoile0626@naver.com>
2026-01-23 16:40:41 +0900

    .

---
+++ src/Programmers/Solution_Pro12954.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class Solution_Pro12954 {                            //x만큼 간격이 있는 n개의 숫자
+    class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+
+            answer[0] = x;
+            for(int i = 1; i < n ; i++){
+                answer[i] = answer[i-1] + x;
+            }
+
+            return answer;
+        }
+    }
+}
