232e110921a795b382d96382743cc06d1c8bc30f
etoile0626 <etoile0626@naver.com>
2026-01-25 22:54:38 +0900

    나머지가 1이 되는 수 찾기

--- src/Programmers/Solution_Pro87389.java
+++ src/Programmers/Solution_Pro87389.java
@@ -1,4 +1,10 @@
 package Programmers;
 
+/*
+    n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 구하기
+    *답이 항상 존재함은 증명될 수 있음.
+ */
+
 public class Solution_Pro87389 {                            //나머지가 1이 되는 수 찾기
+
 }
