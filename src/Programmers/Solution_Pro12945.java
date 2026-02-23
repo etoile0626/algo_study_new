8ebfac605a361dc75716749fe0a68196746946b3
etoile0626 <etoile0626@naver.com>
2026-02-23 15:03:56 +0900

    피보나치 수

--- src/Programmers/Solution_Pro12945.java
+++ src/Programmers/Solution_Pro12945.java
@@ -11,9 +11,11 @@ public class Solution_Pro12945 {                            //피보나치 수
             int[] dp = new int[n+1];
             dp[0] = 0;
             dp[1] = 1;
+
             for(int i = 2; i < n+1; i++){
                 dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
             }
+
             int answer = dp[n];
             return answer;
         }
