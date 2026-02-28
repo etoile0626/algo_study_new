6210e5ba19afe47797199846460ae4f6a8c58af3
etoile0626 <etoile0626@naver.com>
2026-02-28 21:30:04 +0900

    .

---
+++ src/Programmers/Solution_Pro12937.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+/*
+
+ */
+
+public class Solution_Pro12937 {                        //짝수와 홀수
+    class Solution {
+        public String solution(int num) {
+            String answer;
+
+            if(num % 2 == 0){
+                answer = "Even";
+            } else{
+                answer = "Odd";
+            }
+
+            return answer;
+        }
+    }
+}
