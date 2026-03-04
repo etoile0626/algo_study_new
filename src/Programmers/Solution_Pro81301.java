a5e86e87b3888430bbaf43cfd59918c5dee54dea
etoile0626 <etoile0626@naver.com>
2026-03-04 13:05:29 +0900

    숫자 문자열과 영단어

---
+++ src/Programmers/Solution_Pro81301.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+/*
+    숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 구하기
+ */
+
+public class Solution_Pro81301 {                                                //숫자 문자열과 영단어
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+
+            s = s.replace("zero", "0");
+            s = s.replace("one", "1");
+            s = s.replace("two", "2");
+            s = s.replace("three", "3");
+            s = s.replace("four", "4");
+            s = s.replace("five", "5");
+            s = s.replace("six", "6");
+            s = s.replace("seven", "7");
+            s = s.replace("eight", "8");
+            s = s.replace("nine", "9");
+
+            answer = Integer.parseInt(s);
+
+            return answer;
+        }
+    }
+}
