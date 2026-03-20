6cdc89ff7e677867cfeba2f65bb163f40d9fc571
etoile0626 <etoile0626@naver.com>
2026-03-21 00:25:10 +0900

    이상한 문자 만들기

---
+++ src/Programmers/Solution_Pro12930.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+public class Solution_Pro12930 {                        //이상한 문자 만들기
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+
+            String[] str = s.split("");
+
+            int key = 0;
+
+            for(int i=0; i<str.length; i++) {
+                if(key % 2 == 0) {
+                    str[i] = str[i].toUpperCase();
+                }
+
+                if(str[i].isBlank()) {
+                    key = -1;
+                }
+
+                answer += str[i];
+                key++;
+            }
+
+            return answer;
+        }
+    }
+}
