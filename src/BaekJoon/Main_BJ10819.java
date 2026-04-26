bb47409a926cecdb21879768c7c0af45fdead514
etoile0626 <etoile0626@naver.com>
2026-04-26 17:11:32 +0900

    .

---
+++ src/BaekJoon/Main_BJ10819.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+/*
+    N개의 정수로 이루어진 배열 A가 주어진다.
+    이때, 배열에 들어있는 정수의 순서를 적절히 바꿔서 다음 식의 최댓값을 구하는 프로그램을 작성하시오.
+    |A[0] - A[1]| + |A[1] - A[2]| + ... + |A[N-2] - A[N-1]|
+ */
+
+public class Main_BJ10819 {                                          //차이를 최대로
+    public static void main(String[] args) {
+
+    }
+}
