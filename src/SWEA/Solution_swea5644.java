506b742e5458e3b0a4c999e3befec69600eccba9
etoile0626 <etoile0626@naver.com>
2026-05-29 19:42:02 +0900

    .

---
+++ src/SWEA/Solution_swea5644.java
@@ -0,0 +1,14 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+
+ */
+
+public class Solution_swea5644 {                    //[모의 SW 역량테스트] 무선 충전
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
