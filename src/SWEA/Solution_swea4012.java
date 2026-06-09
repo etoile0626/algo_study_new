6a619b420576ccbf90aa8f98d1138cb7f94a8570
etoile0626 <etoile0626@naver.com>
2026-06-09 22:39:15 +0900

    .

--- src/SWEA/Solution_swea4012.java
+++ src/SWEA/Solution_swea4012.java
@@ -2,6 +2,7 @@ package SWEA;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /*
 
@@ -10,5 +11,8 @@ import java.io.InputStreamReader;
 public class Solution_swea4012 {                                    //[모의 SW 역량테스트] 요리사
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
     }
 }
