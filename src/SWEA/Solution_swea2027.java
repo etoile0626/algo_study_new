290b04b474d453fdf8afb7d8c641957d4d8a6d39
etoile0626 <etoile0626@naver.com>
2026-06-27 09:48:28 +0900

    .

---
+++ src/SWEA/Solution_swea2027.java
@@ -0,0 +1,7 @@
+package SWEA;
+
+public class Solution_swea2027 {                        //대각선 출력하기
+    public static void main(String[] args) {
+        System.out.println("#++++\n+#+++\n++#++\n+++#+\n++++#");
+    }
+}
