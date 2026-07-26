5af2ea81310953e3a6f1216bef4b95b64c6c02ce
etoile0626 <etoile0626@naver.com>
2026-07-26 09:09:12 +0900

    .

---
+++ src/SWEA/Solution_swea7465.java
@@ -0,0 +1,26 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+
+ */
+
+public class Solution_swea7465 {                            //창용 마을 무리의 개수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 1 ; t <= T; t++){
+
+            sb.append("#");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
