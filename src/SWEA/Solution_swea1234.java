c976e791b23a5def3ba26317e9dd9b3341c3e984
etoile0626 <etoile0626@naver.com>
2026-07-29 10:34:49 +0900

    .

---
+++ src/SWEA/Solution_swea1234.java
@@ -0,0 +1,11 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_swea1234 {                        //[S/W 문제해결 기본] 10일차 - 비밀번호
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
