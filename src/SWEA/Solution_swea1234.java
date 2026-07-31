7fd658a462acd4f20e882342eb56b4f6fe901322
etoile0626 <etoile0626@naver.com>
2026-07-31 10:08:25 +0900

    비밀번호

--- src/SWEA/Solution_swea1234.java
+++ src/SWEA/Solution_swea1234.java
@@ -1,6 +1,7 @@
 package SWEA;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -10,14 +11,26 @@ import java.util.StringTokenizer;
  */
 
 public class Solution_swea1234 {                        //[S/W 문제해결 기본] 10일차 - 비밀번호
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         for(int t = 1; t <= 10; t++){
+            st = new StringTokenizer(br.readLine());
+            int len = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
 
-            sb.append("#").append(t).append(" ").append().append("\n");
+            for(int j = 0; j < len; j++){                                   //대충 최초 문자열 길이만큼 반복하긴 했는데 더 줄일 방법이 없나?
+                for(int i = 0; i < str.length()-1; i++){
+                    if(str.charAt(i) == str.charAt(i+1)){
+                        str = str.substring(0, i) + str.substring(i+2);
+                        break;
+                    }
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(Integer.parseInt(str)).append("\n");
         }
 
         System.out.println(sb.toString());
