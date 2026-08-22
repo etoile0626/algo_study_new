324d7adb7539a94b48db7cdcd70d5c32785164f4
etoile0626 <etoile0626@naver.com>
2026-08-23 08:39:38 +0900

    .

---
+++ src/SWEA/Solution_swea3307.java
@@ -0,0 +1,22 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_swea3307 {                        //최장 증가 부분 수열
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++){
+
+            sb.append("#").append(t).append(" ").append().append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
